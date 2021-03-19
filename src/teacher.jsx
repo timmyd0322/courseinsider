445715de11e22e12eb577eeafe9c103e95dec674
timmyd0322 <42551641+timmyd0322@users.noreply.github.com>
2021-03-19 09:13:54 -0400

    template of course and instructor first version

--- src/teacher.jsx
+++ src/teacher.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import Navigationbar from './navbar';
-const TeacherPage = () => {
+
+
+export class TeacherPage extends React.Component { 
+  render(){
     return (
       <div>
         <Navigationbar/>
-        <h1>TeacherPage</h1>
+        <div class="wrapper">
+      <div class="one"><center><h1>TeacherName</h1>
+      </center>
+      </div>
+      <div class="two"><center>Two
+      </center>
+      </div>
+      <div class="three"><center>Three
+      </center>
+      </div>
+      <div class="four"><center>Four
+      </center>
+      </div>
+      <div class="five"><center>Five
+      </center>
+      </div>
+      <div class="six"><center>Six
+      </center>
+      </div>
+      </div>
       </div>
 
-  );
-};
+      );
+    }
+}
 
 export default TeacherPage;
