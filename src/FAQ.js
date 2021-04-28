a7ccec82e43f54d82200b7136caa6c2050bc7fe0
Tiger Gamble <46760014+twqfst@users.noreply.github.com>
2021-04-28 19:53:48 -0400

    Updated FAQ pag for now
    FAQ page working on expansion

--- src/FAQ.js
+++ src/FAQ.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import Navigationbar from './navbar';
+import React, { Component } from "react";
+import Navigationbar from "./navbar";
+import {
+    ListGroup,
+  } from "react-bootstrap";
 
-import BottomNavigationbar from './bottomnavbar';
+import BottomNavigationbar from "./bottomnavbar";
 
 const FAQ = () => {
-  
-    return (
-<div>
-<div>
-    <Navigationbar/>
-</div>
+  return (
+    <div>
+      <div>
+        <Navigationbar />
+      </div>
 
-<div>
+      <div>
+        <ListGroup>
+          <ListGroup.Item>What is the this software about?</ListGroup.Item>
+          <ListGroup.Item>What happens with my data?</ListGroup.Item>
+          <ListGroup.Item>Who can use this software?</ListGroup.Item>
+          <ListGroup.Item>Who made this?</ListGroup.Item>
+          <ListGroup.Item>How much does it cost to use this software?</ListGroup.Item>
+        </ListGroup>
+      </div>
 
-</div>
+      <div>
+        <BottomNavigationbar />
+      </div>
+    </div>
+  );
+};
 
-<div>
-    <BottomNavigationbar/>
-</div>
-</div>
-        );
-    };
-    
-    export default FAQ;
+export default FAQ;
